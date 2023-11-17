aee77d6d5d6ed73626f7ab2a913dfdb250f4f2a2
Gonz0179 <gonzalofabiansendra@gmail.com>
2023-11-16 23:00:13 -0300

    Todo nuevo 16/11
    Todo nuevo

--- prueba_arduino/prueba_arduino.ino
+++ prueba_arduino/prueba_arduino.ino
@@ -1,28 +1,27 @@
 #include "SD.h"
-#include <SPI.h> 
 #define SD_ChipSelectPin 4
 #include "TMRpcm.h"
+#include "SPI.h"
 
-TMRpcm tmrpcm; // DECLARAMOS tmrpcm como una variable y ast poder utilizarlo en el codigo
+TMRpcm tmrpcm;
 
-void setup() {
-tmrpcm.speakerPin =9;
-tmrpcm.quality(1);
-tmrpcm.setVolume(5);// aqui se conectara la bocana uno en negativo y el otro a 9 digital
-Serial.begin (9600);
- if (!SD.begin(SD_ChipSelectPin)) {  // Comprueba si la tarjeta SD se ha inicializado correctamente
-    Serial.println("Fallo de la tarjeta SD");  
-    return;   // Si la tarjeta SD falla, el programa termina
-  }
-  else{
-    Serial.println("SD inicializada correctamente!");
-      }
-
+void setup(){
+tmrpcm.speakerPin = 9;
+Serial.begin(9600);
+if (!SD.begin(SD_ChipSelectPin)) {
+Serial.println("SD fail");
+return;
 }
-void loop() {
-  // put your main code here, to run repeatedly: 
-      tmrpcm.play("prue1.wav");
-      
-      tmrpcm.play("prue2.wav");
 
+tmrpcm.setVolume(5);
+tmrpcm.play("3.wav");
+}
+
+void loop(){  
+tmrpcm.play("1.wav");
+delay(60000);
+tmrpcm.play("2.wav");
+delay(60000);
+tmrpcm.play("3.wav");
+delay(60000);
 }
